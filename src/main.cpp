5fd6ad267436c784ced41e28b93b82fb0a43bbab
ArneDJ <ArneDeJonghe@protonmail.com>
2021-03-12 12:21:13 +0100

    window color

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <GL/glew.h>
+#include <GL/gl.h>
 
 int main(int argc, char *argv[])
 {
@@ -14,10 +16,22 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	GLenum error = glewInit();
+	if (error != GLEW_OK) {
+		return 1;
+	}
+
+	glClearColor(1.f, 0.f, 1.f, 1.f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	// The window is open: could enter program loop here (see SDL_PollEvent())
+	SDL_GL_SwapWindow(window);
 
 	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
 
+	SDL_GL_DeleteContext(glcontext);
 	// Close and destroy the window
 	SDL_DestroyWindow(window);
 
